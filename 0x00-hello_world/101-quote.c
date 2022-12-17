e3299f08bed01a9dc0e66d07da14e0f822e64cd0
Gessese Teshome <gessesetey@gmail.com>
2022-12-17 12:57:23 -0800

    yttask 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdlib>
+/**
+ * main -UNIX is basically a simple operating system
+ * Return: 0 on success
+*/
+int main(void)
+{
+	system("echo 'and that piece of art is useful\"\ - Dora Korpar, 2015-10-19'");
+	return (1);
+}
