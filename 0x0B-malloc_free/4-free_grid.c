d0e7c74cafce91316b42b20395f77e5a4af53e84
Gessese Teshome <gessesetey@gmail.com>
2023-01-10 05:04:33 -0800

    completed task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
 #include <stdlib.h>
 
+#include "main.h"
+
 /**
- * free_grid - Frees a 2-dimensional array of integers.
+ * free_grid - use free function on previous grid
  *
- * @grid: The 2-dimensional array of integers to be freed.
- * @height: The height of grid.
+ * @grid: 2 dim. array to free
+ * @height: input height (size)
+ *
+ * Return: free'd grid
  */
 
 void free_grid(int **grid, int height)
 
 {
 
-	int index;
+	int i;
 
-	for (index = 0; index < height; index++)
-		free(grid[index]);
-	free(grid);
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
 }
